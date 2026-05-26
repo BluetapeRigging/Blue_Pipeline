2112ad3ec9b805daf84f2e3a2f7d8ba4da11bb31
Render de Martes <info@renderdemartes.com>
2026-05-25 18:51:03 -0600

    Add versioning support with central version file

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from Blue_Pipeline.version import __version__, __date__
---
+++ version.py
@@ -0,0 +1,5 @@
+# Blue Pipeline - central version file
+# Update this file to bump the version across the pipeline.
+
+__version__ = "1.0.0"
+__date__    = "25/05/2026"
